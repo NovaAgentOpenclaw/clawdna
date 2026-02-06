6dd3315b75a6e70a476a74a5b1637509ad777ad5
Sanderson <sandersonfonseca@gmail.com>
2026-02-05 23:54:01 -0300

    chore: remove legacy files from repo - keep only ClawDNA project

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-ClawDNA - Evolutionary Agent System
-"""
-from .core import Agent, Genome, Evolution
-from .population import Population
-
-__all__ = ['Agent', 'Genome', 'Evolution', 'Population']
--- src/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Core module for ClawDNA
-"""
-from .agent import Agent
-from .genome import Genome
-from .evolution import Evolution
-
-__all__ = ['Agent', 'Genome', 'Evolution']
